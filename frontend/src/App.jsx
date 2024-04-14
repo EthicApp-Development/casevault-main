cc468fee1b03e19b99ddc648b83ae930e56ee1b2
sofia <slgadea@miuandes.cl>
2024-04-14 19:12:31 -0400

    feat: added two more routes of case creation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar.jsx';
-import Home from './Home.jsx'; // Importa tus componentes correctamente
+import Home from './Home.jsx';
 import UserCases from './MyCases/UserCases.jsx';
 import SearchResults from './SearchResults.jsx';
-import useTabs from './Hooks/UseTabs.jsx';
 import AppContext from './Contexts/AppContext.jsx';
 import { Box } from "@mui/material";
 import CreateCase from './CreationCases/CreateCase.jsx';
+import TextCreator from './CreationCases/TextCreator.jsx';
+import VideoCreator from './CreationCases/Video/VideoCreator.jsx';
 function App() {
 
   return (
@@ -21,8 +22,11 @@ function App() {
           <Routes>
             <Route path="/home" element={<Home />} />
             <Route path="/search/:searchTerm" element={<SearchResults />} />
-            <Route path="/usercases/" element={<UserCases />} />
-            <Route path="/create_case/" element={<CreateCase />} />
+            <Route path="/mycases/" element={<UserCases />} />
+            <Route path="/create_case" element={<CreateCase />}>
+              <Route path="text" element={<TextCreator />} />
+              <Route path="videos" element={<VideoCreator />} />
+            </Route>
           </Routes>
         </Box>
       </AppContext.Provider>
