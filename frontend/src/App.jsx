aef824936202e479a45bf9b565bbaf6140b7b8af
jorgepdlr00 <jplaza@buk.cl>
2024-04-25 00:28:46 -0400

    feat: add cases with title and description

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './Home.jsx';
 import UserCases from './MyCases/UserCases.jsx';
 import SearchResults from './SearchResults.jsx';
 import AppContext from './Contexts/AppContext.jsx';
-import { Box } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 import CreateCase from './CreationCases/CreateCase.jsx';
 import TextCreator from './CreationCases/Texto/TextCreator.jsx';
 import VideoCreator from './CreationCases/Video/VideoCreator.jsx';
@@ -16,27 +16,28 @@ import InfoCreator from './CreationCases/Information/InfoCreator.jsx';
 
 function App() {
   const location = useLocation();
-  const { pathname } = location
+  const { pathname } = location;
+
   return (
     <div>
-      <AppContext.Provider value={{
-
-      }}>
-        {pathname !== "/login/" && pathname !== "/login" && <Navbar />}
-        <Box marginTop={20}>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/search/:searchTerm" element={<SearchResults />} />
-            <Route path="/mycases/" element={<UserCases />} />
-            <Route path="/create_case" element={<CreateCase />}>
-              <Route path="text" element={<TextCreator />} />
-              <Route path="videos" element={<VideoCreator />} />
-              <Route path="documents" element={<DocumentCreator />} />
-              <Route path="audios" element={<AudioCreator />} />
-              <Route path="information" element={<InfoCreator />} />
-            </Route>
-          </Routes>
+      <AppContext.Provider value={{}}>
+        <Box sx={{ display: 'flex' }}>
+          {pathname !== "/login/" && pathname !== "/login" && <Navbar />}
+          <Box sx={{ marginRight: pathname !== "/login/" && pathname !== "/login" ? 0 : 0, flexGrow: 2 }}>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/search/:searchTerm" element={<SearchResults />} />
+              <Route path="/mycases/" element={<UserCases />} />
+              <Route path="/create_case" element={<CreateCase />}>
+                <Route path="text" element={<TextCreator />} />
+                <Route path="videos" element={<VideoCreator />} />
+                <Route path="documents" element={<DocumentCreator />} />
+                <Route path="audios" element={<AudioCreator />} />
+                <Route path="information" element={<InfoCreator />} />
+              </Route>
+            </Routes>
+          </Box>
         </Box>
       </AppContext.Provider>
     </div>
--- frontend/src/CreationCases/Texto/TextCreator.jsx
+++ frontend/src/CreationCases/Texto/TextCreator.jsx
@@ -1,12 +1,48 @@
-import { Drawer, Box, List, ListItem, ListItemButton, Button, Typography } from '@mui/material'
+import { Drawer, Box, List, ListItem, ListItemButton, Button, Typography, TextField } from '@mui/material'
+import { styled } from '@mui/material/styles';
 import RTE from '../../Utils/RTE'
 import { useState, useContext } from 'react';
 import React from 'react';
 import { useCaseContext } from '../CreateCase';
 
+const SaveCaseButton = styled(Button)({
+    position: 'absolute',
+    bottom: '16px',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    color: 'white',
+    backgroundColor: '#282828',
+    '&:hover': {
+        color: '#282828',
+        backgroundColor: 'white',
+    },
+});
+
+const CASES_API = import.meta.env.VITE_API_CASES_URL;
+
 export default function TextCreator() {
     const [open, setOpen] = React.useState(false);
-    const { text, setText } = useCaseContext();
+    const { text, setText, title, setTitle } = useCaseContext();
+
+    const handleSave = async () => {
+        try {
+            const response = await fetch(CASES_API, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ title: title, description: text })
+            });
+    
+            if (response.ok) {
+                console.log('Guardado exitoso');
+            } else {
+                console.error('Error al guardar:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error al procesar la solicitud:', error);
+        }
+    };
 
     const toggleDrawer = (newOpen) => () => {
         setOpen(newOpen);
@@ -45,13 +81,29 @@ export default function TextCreator() {
 
     return (
         <Box marginTop={5}>
-            <Button onClick={toggleDrawer(true)}>Cambiar sección de texto</Button>
-            <Box marginTop={3}>
+            <Typography variant="h5" textAlign='center' gutterBottom>
+                Creación de casos
+            </Typography>
+            <TextField
+                label="Título"
+                variant="outlined"
+                fullWidth
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <Box marginTop={2}>
+                <Typography variant="subtitle1" gutterBottom>
+                    Descripción
+                </Typography>
+            </Box>
+            <Box marginTop={1}>
                 <RTE text={text} setText={setText} />
             </Box>
             <Drawer open={open} onClose={toggleDrawer(false)}>
                 {DrawerList}
             </Drawer>
+            <Button onClick={toggleDrawer(true)}>Cambiar sección de texto</Button>
+            <SaveCaseButton onClick={handleSave}>Guardar</SaveCaseButton>
         </Box >
     );
 }
