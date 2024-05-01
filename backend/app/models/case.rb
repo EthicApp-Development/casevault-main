f7ee21b03018de4c1c4bfb6303e24e7f9358b819
jorgepdlr00 <jplaza@buk.cl>
2024-05-01 19:59:29 -0400

    chore: added main image to case model

--- backend/app/models/case.rb
+++ backend/app/models/case.rb
@@ -5,6 +5,15 @@ class Case < ApplicationRecord
     has_many :documents, dependent: :destroy
     has_many :audios, dependent: :destroy
 
+    has_one_attached :main_image
+
     accepts_nested_attributes_for :images, :documents, :audios, :videos, allow_destroy: true
-    
+
+    after_commit :generate_variations, on: [:create, :update]
+
+    private
+
+    def generate_variations
+        main_image.variant(resize: "300x300", format: :webp).processed
+    end
 end  
