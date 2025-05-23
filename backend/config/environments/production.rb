d3cbc22d53d8be8406d27d4f30ad2547299e922b
Nicolas Apara <ncapara@miuandes.cl>
2025-05-22 22:28:20 -0400

    config cache store in production.

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -59,6 +59,12 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, {
+  url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" },
+  namespace: 'cache',
+  expires_in: 1.hour
+  }
+  config.action_controller.perform_caching = true
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
