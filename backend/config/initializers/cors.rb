b56ff47c3ac929051f018bd806031a7d4b5543f0
Nicolas Apara <ncapara@miuandes.cl>
2025-07-17 20:19:06 -0400

    CORS and port config

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   if Rails.env.production?
     allow do
-      origins '*' # Production frontend domain
+      origins ENV.fetch('CORS_ORIGIN', 'https://casevault.ethicapp.info')
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
