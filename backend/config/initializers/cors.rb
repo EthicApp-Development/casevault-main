021e2cbbb558182cf8f4af232975345b61b3c7a6
Nicolas Apara <ncapara@miuandes.cl>
2025-07-22 17:36:33 -0400

    Fixed production cors config

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -12,7 +12,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        expose: ['access-token', 'expiry', 'token-type', 'Authorization']
+        expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
+        credentials: true
     end
   else
     allow do
